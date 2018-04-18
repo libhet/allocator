f61cd3330a74411b9cd497d1815b3c66d7c52727
Denis Shevchenko <cookie-monstr@live.com>
2018-04-18 22:16:43 +0300

    try catch

--- main.cpp
+++ main.cpp
@@ -8,58 +8,59 @@
 
 int main() {
 
-    /**
-     * Создание экземпляра std::map
-     * Заполнение 10 элементами, где ключ это чисо от 0 до 9, а значение - факториал ключа.
-     */
-    {
-        std::map<int,int> std_map;
-        for(int i = 0; i < 10; ++i) {
-            std_map.insert(std::make_pair(i,factorial(i)));
+    try {
+        /**
+         * Создание экземпляра std::map
+         * Заполнение 10 элементами, где ключ это чисо от 0 до 9, а значение - факториал ключа.
+         */
+        {
+            std::map<int, int> std_map;
+            for (int i = 0; i < 10; ++i) {
+                std_map.insert(std::make_pair(i, factorial(i)));
+            }
         }
-    }
 
-    /**
-     * Создание экземпляра std::map с новым аллокатором ограниченным 10 элементами,
-     * заполнение 10 элементами, где ключ это число от 0 до 9, а значение - факториал ключа.
-     * Вывод на экран всех значений (ключ и значение разделены пробелом) хранящихся в контейнере.
-     */
-    {
-        std::map<int, int, std::less<int>, my::allocator<std::pair<const int, int>,10>> std_map_with_my_allocator;
-        for(int i = 0; i < 10; ++i) {
-            std_map_with_my_allocator.insert(std::make_pair(i,factorial(i)));
+        /**
+         * Создание экземпляра std::map с новым аллокатором ограниченным 10 элементами,
+         * заполнение 10 элементами, где ключ это число от 0 до 9, а значение - факториал ключа.
+         * Вывод на экран всех значений (ключ и значение разделены пробелом) хранящихся в контейнере.
+         */
+        {
+            std::map<int, int, std::less<int>, my::allocator<std::pair<const int, int>, 10>> std_map_with_my_allocator;
+            for (int i = 0; i < 10; ++i) {
+                std_map_with_my_allocator.insert(std::make_pair(i, factorial(i)));
+            }
+            for (auto _pair : std_map_with_my_allocator) {
+                std::cout << _pair.first << " " << _pair.second << std::endl;
+            }
         }
-        for(auto _pair : std_map_with_my_allocator) {
-            std::cout << _pair.first << " " << _pair.second << std::endl;
-        }
-    }
 
-    /**
-     * Создание экземпляра своего контейнера для хранения int
-     * заполнение 10 элементами от 0 до 9
-     */
-    {
-        my::forward_list<int> my_forward_list_with_std_allocator;
-        for(int i = 0; i < 10; ++i) {
-            my_forward_list_with_std_allocator.append(i);
+        /**
+         * Создание экземпляра своего контейнера для хранения int
+         * заполнение 10 элементами от 0 до 9
+         */
+        {
+            my::forward_list<int> my_forward_list_with_std_allocator;
+            for (int i = 0; i < 10; ++i) {
+                my_forward_list_with_std_allocator.append(i);
+            }
         }
-    }
 
-    /**
-     * Создание экземпляра своего контейнера для хранения int
-     * с новым аллокатором ограниченным 10 элементами,
-     * заполнение 10 элементами от 0 до 9.
-     * Вывод на экран всех значений хранящихся в контейнере.
-     */
-    {
-        my::forward_list<int, my::allocator<int,10>> my_fl_with_my_allocator;
-        for(int i = 0; i < 10; ++i) {
-            my_fl_with_my_allocator.append(i);
+        /**
+         * Создание экземпляра своего контейнера для хранения int
+         * с новым аллокатором ограниченным 10 элементами,
+         * заполнение 10 элементами от 0 до 9.
+         * Вывод на экран всех значений хранящихся в контейнере.
+         */
+        {
+            my::forward_list<int, my::allocator<int, 10>> my_fl_with_my_allocator;
+            for (int i = 0; i < 10; ++i) {
+                my_fl_with_my_allocator.append(i);
+            }
+            for (auto _int : my_fl_with_my_allocator) {
+                std::cout << _int << std::endl;
+            }
         }
-        for(auto _int : my_fl_with_my_allocator) {
-            std::cout << _int << std::endl;
-        }
-    }
 
 //    /**
 //     * Map Test Block
@@ -101,6 +102,11 @@ int main() {
 //        for(auto item:list_with_my_allocator) std::cout << item << " ";
 //        std::cout << std::endl;
 //    }
+    }
+    catch(const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
 
     return 0;
 }
